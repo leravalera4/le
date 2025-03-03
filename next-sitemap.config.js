2091e26a1a2bb469530f9108fdf0fc8ce673b4fc
leravalera4 <51717886+leravalera4@users.noreply.github.com>
2025-03-03 15:31:04 -0500

    Add files via upload

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://shoppyscan.ca', // Укажите основной URL вашего сайта
+    generateRobotsTxt: true, // Создаёт robots.txt, включающий ссылку на карту сайта
+    sitemapSize: 7000, // Максимальное количество URL в одном файле карты сайта
+  }
+  
